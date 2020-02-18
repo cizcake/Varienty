d471f52f34ed4b3de06c15fe6f2e26ca442b9963
cizcake <31511841+cizcake@users.noreply.github.com>
2020-02-18 14:26:23 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,60 @@
+$(function() 
+ { 
+
+ 	// Fixed Header
+  let header = $("#header");
+  let intro = $("#intro");
+  let introH = intro.innerHeight();
+  let scrollPos = $(window).scrollTop();
+  
+  checkScroll(scrollPos,introH);
+
+  $(window).on("scroll resize", function()
+  {
+
+    introH = intro.innerHeight();
+    scrollPos = $(this).scrollTop();
+    
+    checkScroll(scrollPos,introH);
+
+  });
+
+ function checkScroll(scrollPos,introH)
+ {
+
+ if( scrollPos > introH)
+    {
+        header.addClass("fixed");
+    }
+    else
+    {
+       header.removeClass("fixed");
+    }
+
+ }
+  
+// Smooth Scroll
+
+$("[data-scroll]").on("click", function(event)
+
+{
+
+event.preventDefault();
+
+let elementId= $(this).data('scroll');
+let elementOffset = $(elementId).offset().top;
+
+$("html, body").animate({
+	scrollTop: elementOffset - 70
+}, 700);
+
+});
+
+// Nav Toggle
+let nav = $("nav");
+$("#navToggle").on("click", function (event){
+event.preventDefault();
+nav.toggleClass("show");
+});
+
+ });
